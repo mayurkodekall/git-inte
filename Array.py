2c7a658d98752200d528e6ee05ebb9b6c3f1349f
Mayur <mayurkodekall@gmail.com>
2020-11-22 22:36:41 +0530

    committed

---
+++ Array.py
@@ -0,0 +1,21 @@
+'''def sum(arr,n):
+    arr=[1,2,3]
+    c=sum(arr)
+    print(c)
+    sum(arr,n)'''
+
+'''def sumz(arr):
+    arr=([1,2],[2,4])
+    
+
+
+
+
+
+    b=sumz(arr)
+    print(arr)'''
+for i in range(0,4):
+    for j in range(i+1):
+        print("*",end="")
+    print("")
+
